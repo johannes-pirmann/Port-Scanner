877ff7a24af0dcf55f0236b35b053aaa92143b5a
johannes-pirmann <johannes.pirmann@gmail.com>
2021-03-18 11:19:34 +0100

    Added progressbar
    Signed-off-by: johannes-pirmann <johannes.pirmann@gmail.com>

--- scanner.py
+++ scanner.py
@@ -1,16 +1,39 @@
 from socket import *
 import time
+import sys
+from threading import Thread, Event
+
+
 starting_time = time.time()
 system_ports = (0,1023)
 user_ports = (1024, 49151)
 
 
-def testport(ip_address, port_address):
+def testport(ip_address, port_address, open_ports):
     s = socket(AF_INET, SOCK_STREAM)
     conn = s.connect_ex((ip_address, port_address))
     if conn == 0:
-        print(f'Port {port_address} is open.')
+        open_ports.append(port_address)
+        #print(f'Port {port_address} is open.')
     s.close()
+    return open_ports
+
+
+
+def progress(count, total, status=''):
+    """
+    Progressbar by: vladignatyev
+    Source: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+    """
+    bar_len = 50
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total),1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
 
 
 if __name__ == '__main__':
@@ -24,8 +47,11 @@ if __name__ == '__main__':
     else:
         print('wrong input!')
 
+    open_ports = []
     for i in range(use_ports[0],use_ports[1]):
-        testport(ip_address, i)
-
+        
+        testport(ip_address, i, open_ports)
+        progress(i, use_ports[1], 'Scanning Ports')
+    print(f'\nOpen Ports: {open_ports}')
 
 print(f'It took {time.time()-starting_time}')
